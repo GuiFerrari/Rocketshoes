f8b0781f4875ca999bb93bf113e812426fa7e0db
Guilherme Ferrari <guiferrari10@hotmail.com>
2019-11-18 00:00:23 -0300

    Criado reducer do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
@@ -8,6 +9,8 @@ import {
 import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
+  const products = useSelector(state => state[0].products);
+
   return (
     <Container>
       <ProductTable>
@@ -21,37 +24,36 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-couro-oakley-modoc-low-30-masculino/26/D63-5017-026/D63-5017-026_zoom1.jpg"
-                alt="Tênis"
-              />
-            </td>
-            <td>
-              <strong>Tênis Oakley</strong>
-              <span>R$ 549,90</span>
-            </td>
-            <td>
-              <div>
+          {products.map(product => (
+            <tr key={product.id}>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$ 549,90</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={20} color="7159c1" />
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input type="number" readOnly value={1} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$ 549,90</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
       <footer>
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -8,7 +8,24 @@ export default function cart(state = INITIAL_STATE, action) {
   return produce(state, draft => {
     switch (action.type) {
       case '@cart/ADD_TO_CART': {
-        draft.products = [...state.products, action.payload.product];
+        const productIndex = draft.products.findIndex(
+          p => p.id === action.payload.product.id
+        );
+        if (productIndex >= 0) {
+          draft.products[productIndex].amount += 1;
+        } else {
+          draft.products.push({
+            ...action.payload.product,
+            amount: 1,
+          });
+        }
+        // draft.products = [
+        //   ...state.products,
+        //   {
+        //     ...action.payload.product,
+        //     amount: 1,
+        //   },
+        // ];
         break;
       }
       default:
